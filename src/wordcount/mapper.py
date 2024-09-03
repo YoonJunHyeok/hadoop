20e6064e5831e264b4ad2b11e32435d5961168be
YoonJunHyeok <jhyoon1607@gmail.com>
2024-09-04 00:33:38 +0900

    fix code

--- src/wordcount/mapper.py
+++ src/wordcount/mapper.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import sys
 
+first_line = True
+
 for line in sys.stdin:
+    if first_line:
+        first_line = False
+        continue
+
     values = line.strip().split(",")
 
     print(f"{values[7]}\t1")
